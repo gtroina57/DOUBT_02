03064e8e1160b5745e22af5d4e515ca63fb0eb99
gtroina57 <50884551+gtroina57@users.noreply.github.com>
2025-07-01 12:53:31 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from autogen_agentchat.teams import SelectorGroupChat
 from autogen_agentchat.messages import HandoffMessage
 from autogen_agentchat.teams import Swarm
 from autogen_core.tools import FunctionTool
-from autogen.agentchat.message_utils import convert_to_messages
+from autogen_agentchat.message_utils import convert_to_messages
 from typing import Sequence
 from typing import Any, Dict, List
 
